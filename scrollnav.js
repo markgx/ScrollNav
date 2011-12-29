a34240665de25c1e0e943d8139d2f74ea81e8278
Mark Guerra <markgx@gmail.com>
2011-12-28 20:31:20 -0800

    scroll on hashchange

--- scrollnav.js
+++ scrollnav.js
@@ -12,25 +12,27 @@
   scrollNav.prototype.init = function () {
     $el = $(this.element);
 
-    $el.find('a').on('click', function(e) {
-      e.preventDefault();
-
-      var id = $(this).attr('href').slice(1);
-      scrollTo(id);
+    $(window).bind('hashchange', function(e) {
+      checkHash();
     });
+
+    checkHash();
   };
 
   $.fn.scrollNav = function (options) {
     return new scrollNav(this, options);
   }
 
+  function checkHash() {
+    if (window.location.hash.slice(0, 3) === '#!/') {
+      var id = window.location.hash.slice(3);
+      scrollTo(id);
+    }
+  }
+
   function scrollTo(id, ms) {
     $('html, body').animate({
       scrollTop: $('#' + id).offset().top
-    }, {
-      complete: function() {
-        window.location.hash = id;
-      }
     }, ms || 500);
   }
 })(jQuery, window, document);
